9fece612adf8e6ad3bb4145d9f46ad4677cb7cc3
Alex Myers <alex@pek10.dev>
2023-03-24 10:58:11 -0400

    Fixed errors in migration

--- back-end/src/db/migrations/20230323215629_createReservationsTable.js
+++ back-end/src/db/migrations/20230323215629_createReservationsTable.js
@@ -1,16 +1,19 @@
+/* called when migration is run */
 exports.up = function (knex) {
   return knex.schema.createTable('reservations', (table) => {
-    table.increments('reservation_id').primary()
-    table.timestamps(true, true)
+    table.increments('reservation_id').primary().notNullable()
     table.string('first_name', 255).notNullable()
     table.string('last_name', 255).notNullable()
     table.string('mobile_number', 12).notNullable()
-    table.string('reservation_date', 10).notNullable()
-    table.string('reservation_time', 8).notNullable()
+    table.date('reservation_date', 10).notNullable()
+    table.time('reservation_time', 8).notNullable()
     table.integer('people').notNullable()
+    table.string('status')
+    table.timestamps(true, true)
   })
 }
 
+/** called when migration is rolled back */
 exports.down = function (knex) {
   return knex.schema.dropTable('reservations')
 }
